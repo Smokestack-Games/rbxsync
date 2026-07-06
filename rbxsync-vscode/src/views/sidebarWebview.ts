f529109cfc03654952753974c4c5c58567c3fa7b
vexedaa <62490182+vexedaa@users.noreply.github.com>
2026-07-06 17:19:15 -0400

    feat(vscode): treat datamodel.rbxjson as the hidden read-only context file

--- rbxsync-vscode/src/views/sidebarWebview.ts
+++ rbxsync-vscode/src/views/sidebarWebview.ts
@@ -1220,7 +1220,7 @@ export class SidebarWebviewProvider implements vscode.WebviewViewProvider {
       </div>
       <div class="quick-row" id="rbxjsonBtn">
         <svg class="icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M14 2H6a2 2 0 00-2 2v16a2 2 0 002 2h12a2 2 0 002-2V8z"/><polyline points="14 2 14 8 20 8"/></svg>
-        <span class="label">Show .rbxjson</span>
+        <span class="label">Show Context File</span>
         <div class="toggle" id="rbxjsonToggle"></div>
       </div>
     </div>
